79c7a2085d83097798b8e1fb90bded52dfe2a5a4
Sergey Vorotnikov <svorotnikov@upteam.com>
2022-02-06 21:49:19 +0300

    Make city selector component

---
+++ src/components/CitySelector.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="city-selector">
+    <input 
+      v-model="city"
+      class="city-selector--input"
+      @change="getListOfCities"
+    />
+    <template v-if="isListOpened">
+      <div class="city-selector--list city-list">
+        <div
+          v-for="c in cities"
+          class="city-list--item"
+          :key="c.id"
+          @click="selectCity(c)"
+        >
+          {{ c.name }}, {{ c.country }}
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script setup>
+  import { ref, computed } from 'vue'
+  import { getCities } from '@/api/api'
+
+  const emit = defineEmits(['select-city'])
+
+  const city = ref('Saint Petersburg') // id: 498817
+  const cities = ref([])
+  const isListOpened = computed(() => cities.value.length) 
+
+  const getListOfCities = async () => {
+    if (!city.value) {
+      cities.value = [];
+    }
+
+    const rawCities = await getCities(city.value)
+
+    cities.value = rawCities.map(c => ({
+      id: c.id,
+      name: c.name,
+      country: c.country
+    }))
+  }
+
+  const selectCity = (selected) => {
+    city.value = selected.name
+    cities.value = []
+
+    emit('select-city', selected.id)
+  }
+</script>
+
+<style lang="scss" scoped>
+  .city-selector {
+    width: 100%;
+    max-width: 360px;
+    position: relative;
+    &--input {
+      cursor: pointer;
+      width: 100%;
+      padding: 10px;
+      box-sizing: border-box;
+      border: none;
+      border-radius: 4px;
+      color: #fff;
+      background-color: transparent;
+      outline: none;
+      &:focus {
+        background-color: #b276e9;
+        cursor: text;
+      }
+    }
+    &--list {
+      width: 100%;
+      position: absolute;
+      left: 0;
+      top: 100%;
+      border: 2px solid #b276e9;
+      border-radius: 4px;
+      box-sizing: border-box;
+    }
+  }
+  .city-list {
+    padding: 0px;
+    max-height: 300px;
+    overflow: scroll;
+    &--item {
+      padding: 5px;
+      line-height: 20px;
+      &:hover {
+        background-color: aquamarine;
+      }
+    }
+  }
+</style>
